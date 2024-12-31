fba13a61e92e3243101b98b4dae640bcb9e58ba6
Misa Korzun <misakorzun10@gmail.com>
2024-12-31 20:16:33 +0500

    add new stuff

--- Random_game.py
+++ Random_game.py
@@ -1,10 +1,10 @@
 import random
 botNumber=random.randint(1,10)
-playerNumber=int(input("Введите число"))
+playerNumber=int(input("Введите число \n"))
 while playerNumber !=botNumber:
     if playerNumber>botNumber:
         print("ты не угадал,мое число меньше")
     else:
         print("ты не угадал,мое число больше")
-        playerNumber=int(input("Введите число \n"))
+        playerNumber=int(input("Введите новое число \n"))
 print("ты угадал мое число:" + str({botNumber}))
